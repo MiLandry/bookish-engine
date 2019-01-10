3193c54e58bf9879d17262ec33288f3655a44524
Michael Landry <mlandryviolin@gmail.com>
2019-01-09 19:20:45 -0500

    asnothie

--- src/App.js
+++ src/App.js
@@ -1,62 +1,41 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import logo from "./logo.svg"
 import "./App.css"
 import FoodTable from "./FoodTable/FoodTable"
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      isLoaded: false,
-      food: {
-        e5d9d9f5: {
-          label: "ice cream",
-          isDelicious: true,
-          isHealthy: false,
-        },
-        a9ba692b: {
-          label: "pizza",
-          isDelicious: true,
-          isHealthy: false,
-        },
-        ze128a47: {
-          label: "spinach",
-          isDelicious: false,
-          isHealthy: true,
-        },
-      },
-    }
-    this.onChange = this.onChange.bind(this)
-  }
+const initialFood = {
+  e5d9d9f5: {
+    label: "ice cream",
+    isDelicious: true,
+    isHealthy: false,
+  },
+  a9ba692b: {
+    label: "pizza",
+    isDelicious: true,
+    isHealthy: false,
+  },
+  ze128a47: {
+    label: "spinach",
+    isDelicious: false,
+    isHealthy: true,
+  },
+}
 
-  componentDidMount() {
-    window.setTimeout(() => {
-      let state = this.state
-      this.setState({ ...state, isLoaded: true })
-    }, 800)
-  }
+function App() {
+  const [food, setFood] = useState(initialFood)
+  const [isLoaded, setIsLoaded] = useState(false)
 
-  onChange(event) {
+  const onChange = event => {
     const { id, checked, name } = event.target
-    let { food } = this.state
     food[id][name] = checked
-
-    this.setState({ ...this.state, food })
+    setFood(food)
   }
-  i
-  render() {
-    const { isLoaded } = this.state
 
-    return (
-      <div className={"App"}>
-        <FoodTable
-          data={this.state.food}
-          onChange={this.onChange}
-          isLoaded={isLoaded}
-        />
-      </div>
-    )
-  }
+  return (
+    <div className={"App"}>
+      <FoodTable data={food} onChange={onChange} isLoaded={isLoaded} />
+    </div>
+  )
 }
 
 export default App
